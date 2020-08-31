a6a92d850d45b6f438b982a34721eda2ef50339e
Anselmo Leonardo <enge.leon@gmail.com>
2020-08-31 20:19:02 -0300

    novos pics

--- BibliotecaGothamAPI/Startup.cs
+++ BibliotecaGothamAPI/Startup.cs
@@ -10,6 +10,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
 
 namespace BibliotecaGothamAPI
 {
@@ -25,12 +26,34 @@ namespace BibliotecaGothamAPI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            // Register the Swagger generator, defining 1 or more Swagger documents
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Biblioteca de Gotham API", Version = "v1",
+                Description = "Uma API para operações de biblioteca de livros.",
+                    Contact = new OpenApiContact
+                    {
+                        Name = "Anselmo Leonardo Silva",
+                        Email = "enge.leon@gmail.com",
+                        Url = new Uri(" https://www.linkedin.com/in/anselmo-silva-100b5826/"),
+                    },
+                });
+            });
             services.AddControllers();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.),
+            // specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+            });
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
